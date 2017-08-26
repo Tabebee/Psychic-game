1c72ccd447039926751daa1ab6faed12ca61fba6
Tabebee <tabibone@gmail.com>
2017-08-26 00:11:00 -0400

    all done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,19 +9,20 @@ var lettersGuessed = [];
 //Computer makes a guess
 var guess = compChoice[Math.floor(Math.random() * compChoice.length)];
 
-
+//function starts when an a key is pressed. (event)
 document.onkeyup = function(event) {
-
+//whatever key is pressed will be registered as user key
     var user = event.key;
-
+//This adds to the letters guessed section on the html. Each guess is stacked next to the previous.
     var lettersGuessedSpan = document.getElementById('lettersGuessed');
     lettersGuessedSpan.textContent = lettersGuessedSpan.textContent + user + ", ";
 
+//How the game will be scored and reset after a win and loss
     if (user === guess) {
         wins++;
         alert("Wow Your Phychic!");
         guessesLeft = 10;
-        lettersGuessed = [];
+        lettersGuessedSpan.textContent = [];
         var newguess = compChoice[Math.floor(Math.random() * compChoice.length)];
         guess = newguess;
 
@@ -29,9 +30,11 @@ document.onkeyup = function(event) {
         losses++;
         alert("Looks like your not Psychic!");
         guessesLeft = 10;
+        lettersGuessedSpan.textContent = [];
         var newguess = compChoice[Math.floor(Math.random() * compChoice.length)];
         guess = newguess;
-
+//this is to make sure the user doesnt get infinity guesses. It will take away from our variable for every attempt
+//until game is either won or lost.
     } else if (user !== guess) {
         guessesLeft--;
     }
